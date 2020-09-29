2d74cf1fbe2eb18c3842ad98f6ebf6170cc63d5f
RISHI_BHARADWAJ <rishi.r.bharadwaj@gmail.com>
2020-09-29 19:48:56 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,62 @@
+class Form 
+{
+
+  constructor() 
+  {
+    
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+    this.reset = createButton("Reset");
+
+  }
+  hide()
+  {
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display()
+  {
+    background(formBg);
+    
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0);
+
+    this.input.position(displayWidth/2 - 520 , displayHeight/2 - 380);
+    this.button.position(displayWidth/2 - 520, displayHeight/2- 350);
+    this.reset.position(displayWidth-300,30);
+
+    this.button.mousePressed(()=>
+    {
+
+    this.input.hide();
+    this.button.hide();
+    player.name = this.input.value();
+    playerCount+=1;
+    player.index = playerCount;
+    player.update();
+    player.updateCount(playerCount);
+    this.greeting.html("Hello " + player.name + "  ! Welcome to the car racing game")
+    this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    })
+
+    this.reset.mousePressed(() =>
+    {
+
+      //reset the database 
+      game.update(0);
+      player.updateCount(0);
+      Player.updateCarsAtEnd(0);
+
+
+
+    })
+
+    
+
+  }
+}
